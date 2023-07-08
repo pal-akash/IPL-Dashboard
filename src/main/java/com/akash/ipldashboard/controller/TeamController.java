c8006e95c57281364f8a69e5dbd6ec4256fe2d75
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-08 21:30:58 +0530

    [feat] fetch team by team name

--- src/main/java/com/akash/ipldashboard/controller/TeamController.java
+++ src/main/java/com/akash/ipldashboard/controller/TeamController.java
@@ -2,13 +2,11 @@ package com.akash.ipldashboard.controller;
 
 import com.akash.ipldashboard.model.Team;
 import com.akash.ipldashboard.repository.TeamRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
@@ -17,7 +15,7 @@ public class TeamController {
         this.teamRepository = teamRepository;
     }
 
-    @GetMapping("/team/{teamName}")
+    @GetMapping("/api/team/{teamName}")
     public Team getTeam(@PathVariable String teamName){
         Team team = this.teamRepository.findByTeamName(teamName);
         return team;
--- src/main/java/com/akash/ipldashboard/repository/TeamRepository.java
+++ src/main/java/com/akash/ipldashboard/repository/TeamRepository.java
@@ -1,2 +1,9 @@
-package com.akash.ipldashboard.repository;public interface TeamRepository {
+package com.akash.ipldashboard.repository;
+
+import com.akash.ipldashboard.model.Team;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+
+    Team findByTeamName(String teamName);
 }

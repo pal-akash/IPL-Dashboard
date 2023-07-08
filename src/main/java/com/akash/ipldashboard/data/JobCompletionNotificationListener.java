5c1a76a063739a3763c1a0eb0abf8171be732c50
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-08 21:03:25 +0530

    Revert "Revert "[refactor]""
    This reverts commit eabde6ed463a50241cda74912cb371dbd7ad20d9.

--- src/main/java/com/akash/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/com/akash/ipldashboard/data/JobCompletionNotificationListener.java
@@ -13,7 +13,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Component
@@ -28,6 +27,13 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
         this.em = em;
     }
 
+//    private final JdbcTemplate jdbcTemplate;
+
+//    @Autowired
+//    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+
     @Override
     @Transactional
     public void afterJob(JobExecution jobExecution) {
@@ -36,26 +42,32 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 
             Map<String, Team> teamData = new HashMap<>();
 
-            em.createQuery("select distinct m.team1, count(*) from Match m group by m.team1", Object[].class)
+            em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
                     .getResultList()
                     .stream()
-                    .map(e -> new Team((String)e[0], (long) e[1]))
+                    .map(e -> new Team((String) e[0], (long) e[1]))
                     .forEach(team -> teamData.put(team.getTeamName(), team));
 
-            em.createQuery("select distinct m.team2, count(*) from Match m group by m.team2", Object[].class)
+            em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
                     .getResultList()
                     .stream()
                     .forEach(e->{
                         Team team = teamData.get((String) e[0]);
                         team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
                     });
-            em.createQuery("select m.matchWinner, count(*) from Match m group by m.match_winner", Object[].class)
+
+            em.createQuery("select m.winningTeam, count(*) from Match m group by m.winningTeam", Object[].class)
                     .getResultList()
                     .stream()
                     .forEach(e->{
                         Team team = teamData.get((String) e[0]);
                         if (team != null) team.setTotalWins((long) e[1]);
                     });
+
+//            For testing
+//            jdbcTemplate.query("SELECT team1, team2, date FROM match",
+//                    (rs, row) -> "Team 1 "  + rs.getString(1) + " Team 2 " + rs.getString(2) + " Date " + rs.getString(3)
+//            ).forEach(str -> System.out.println(str));
 
             teamData.values().forEach(team-> em.persist(team));
             teamData.values().forEach(team-> System.out.println(team));

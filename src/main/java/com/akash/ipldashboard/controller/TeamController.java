a4721cce6e5ed304de5f56b3412478ac4359db3f
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-09 22:42:22 +0530

    [refactor] fetch latest matches by team name

--- src/main/java/com/akash/ipldashboard/controller/TeamController.java
+++ src/main/java/com/akash/ipldashboard/controller/TeamController.java
@@ -3,9 +3,6 @@ package com.akash.ipldashboard.controller;
 import com.akash.ipldashboard.model.Team;
 import com.akash.ipldashboard.repository.MatchRepository;
 import com.akash.ipldashboard.repository.TeamRepository;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +21,10 @@ public class TeamController {
 
     @GetMapping("/api/team/{teamName}")
     public Team getTeam(@PathVariable String teamName){
+
         Team team = this.teamRepository.findByTeamName(teamName);
-        Pageable pageable = PageRequest.of(0,4);
-        team.setMatches(this.matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, pageable));
+
+        team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4));
 
         return team;
     }
